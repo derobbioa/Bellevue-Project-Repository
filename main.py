47c6c4f12f26537b81c0b0d4212d9d008a68ddef
AdamDeRobbio <23745965-AdamDeRobbio@users.noreply.replit.com>
2023-08-01 19:15:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to the Fiber Optic Installation Program!")
+company_name = input("Enter your company name: ")
+feet_of_fiber = float(input("Enter the number of feet of fiber optic to be installed: "))
+cost_per_feet = 0.87
+total_cost = feet_of_fiber * cost_per_feet
+print ("")
+print("CALCULATED INFORMATION")
+print("Company Name:", company_name)
+print("Feet of Fiber Optic to be Installed:", feet_of_fiber)
+print("Total Cost: $", total_cost)
